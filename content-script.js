56af0803732ccabff691407f0f9af2a6eaf36a7d
Jechol Lee <mr.jechol@gmail.com>
2023-06-12 14:56:01 +0900

    reflect design change

--- content-script.js
+++ content-script.js
@@ -1,4 +1,4 @@
-console.log("version: 11")
+console.log("version: 12")
 
 // page size
 
@@ -7,34 +7,61 @@ size_input.value = "9999"
 
 // sort rows
 
-const tbody_sel = '#container > div > div.table.colTable.v2.textCenter.mV2Table > table > tbody'
-const tbody = document.querySelector(tbody_sel)
+const edu_list_sel = '#container > div > div.edu_list'
+const edu_list = document.querySelector(edu_list_sel)
 
 let enable_timout = true;
 let sort_timeout = null;
 
-tbody.addEventListener('DOMNodeInserted', function (event) {
-	if (enable_timout && event.target.parentNode == tbody) {
+edu_list.addEventListener('DOMNodeInserted', function (event) {
+	console.log("DOMNodeInserted")
+	if (enable_timout && event.target.parentNode == edu_list) {
 		clearTimeout(sort_timeout)
 		sort_timeout = setTimeout(sort_rows, 100)
 	}
 });
 
-function sort_rows() {
-	let nStart = new Date().getTime()
+function chunkArray(array, chunkSize) {
+  const chunkedArray = [];
+  for (let i = 0; i < array.length; i += chunkSize) {
+    const chunk = array.slice(i, i + chunkSize);
+    chunkedArray.push(chunk);
+  }
+  return chunkedArray;
+}
 
-	enable_timout = false;
-	let trs = tbody.querySelectorAll('tr')
-	trs = Array.from(trs).sort((a, b) => {
-		const num_a = parseInt(a.querySelector('td:nth-child(8)').textContent)
-		const num_b = parseInt(b.querySelector('td:nth-child(8)').textContent)
-		return num_b - num_a
-	})
-	tbody.replaceChildren(...trs)
-	enable_timout = true;
+function parseIntFromStringMixed(inputString) {
+  const numericPart = inputString.replace(/\D/g, ''); // Remove non-digit characters
+  const parsedInt = parseInt(numericPart, 10); // Parse the numeric part as an integer
+  return parsedInt;
+}
 
-	let nEnd = new Date().getTime()
-	console.log(`sorted ${trs.length} rows in ${nEnd - nStart} ms`)
+function getNumberFromDivGroup(divs) {
+		const text = divs[2].querySelector('div.date_num > p:nth-child(2) > span:nth-child(1)')
+		const num = parseIntFromStringMixed(text.textContent)
+		return num;
 }
 
 
+
+function sort_rows() {
+	console.log("sort_rows")
+	let nStart = new Date().getTime()
+
+	enable_timout = false;
+	let divs = [...edu_list.querySelectorAll('div')]
+	console.log("insid sort divs.length", divs.length)
+	let groups = chunkArray(divs, 3);
+
+	let sorted_groups = Array.from(groups).sort((a, b) => {
+		return getNumberFromDivGroup(b) - getNumberFromDivGroup(a)
+	})
+	let sorted_divs = [].concat(...sorted_groups)
+	edu_list.replaceChildren(...sorted_divs)
+	enable_timout = true;
+
+	let nEnd = new Date().getTime()
+	console.log(`sorted ${divs.length} rows in ${nEnd - nStart} ms`)
+}
+
+sort_rows()
